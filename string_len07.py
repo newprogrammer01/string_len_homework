10cbe5bbd2a905a08568e532bf71d603bb02d9ec
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-23 21:28:33 +0500

    07' git push

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,23 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    if a%2==1 and b%2==1 and c%2==1:
+        return (s1 ,s2, s3)
+    if a%2==0 and b%2==0 and c%2==0:
+        return 0
+    if a%2==0 and b%2==1 and c%2==0:
+         return s2
+    if a%2==0 and b%2==0 and c%2==1:
+        return s3
+    if a%2==1 and b%2==0 and c%2==0:  
+        return s1
+    if a%2==1 and b%2==1 and c%2==0:
+        return s1,s2
+    if a%2==1  and b%2==0 and c%2==1:
+        return s1,s3
+    if a%2==0 and b%2==1 and c%2==1:
+        return s2,s3
+print(main("[code]", "[coder]" , "[python]"))
